6636e229f6ccbcccbde1eae1fa1cb564fdb2135a
Alexandr Nefedov <nefedov19911991@gmail.com>
2026-09-03 16:10:09 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+class Stack:
+    def __init__(self):
+        self.new_list = []
+
+    def is_empty(self):
+        if len(self.new_list) == 0:
+            return True
+        else:
+            return False
+
+    def push(self, item):
+        self.new_list.append(item)
+
+    def pop(self):
+        return self.new_list.pop()
+
+    def peek(self):
+        return self.new_list[len(self.new_list) - 1]
+
+    def size(self):
+        return len(self.new_list)
+
+
+
+def is_balanced(new_str):
+    char_list = list(new_str)
+    my_stack = Stack()
+    for item in char_list:
+        if item == '(' or item == '[' or item == '{':
+            my_stack.push(item)
+        elif item == ')' or item == ']' or item == '}':
+            if my_stack.is_empty():
+                return "Несбалансированно"
+            if my_stack.peek() == '(' and item == ')' or my_stack.peek() == '[' and item == ']' or my_stack.peek() == '{' and item == '}':
+                my_stack.pop()
+            else:
+                return "Несбалансированно"
+    if my_stack.is_empty():
+        return "Сбалансированно"
+    else:
+        return "Несбалансированно"
+
+print(is_balanced('([{({})}])'))
